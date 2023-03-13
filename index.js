a70fda62695c3fbd604a9385ac06742eed9d7466
Yehor Spyrydonov <extremecode0xff@gmail.com>
2023-03-13 13:35:48 +0200

    Added http server page

--- index.js
+++ index.js
@@ -1,6 +1,58 @@
-const chalk = require('chalk');
-const GREETINGS = require('./data');
-const { directory } = require('./demo/path');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-console.log(chalk.blue(GREETINGS));
-console.log(directory);
+const server = http.createServer((req, res) => {
+  let filePath = path.join(
+    __dirname,
+    'public',
+    req.url === '/' ? 'index.html' : req.url,
+  );
+  const ext = path.extname(filePath);
+  let contentType = 'text/html';
+
+  switch (ext) {
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    default:
+      contentType = 'text/html';
+  }
+
+  if (!ext) {
+    filePath += '.html';
+  }
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      fs.readFile(
+        path.join(__dirname, 'public', 'error.html'),
+        (readErr, data) => {
+          if (readErr) {
+            res.writeHead(500);
+            res.end('Error');
+          } else {
+            res.writeHead(200, {
+              'Content-Type': 'text/html',
+            });
+            res.end(data);
+          }
+        },
+      );
+    } else {
+      res.writeHead(200, {
+        'Content-Type': contentType,
+      });
+      res.end(content);
+    }
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server has been started on ${PORT}...`);
+});
